d46a6f184e66a0d2320c191a08f48d2d815326bd
LasseBP <lasse@huhej.dk>
2014-05-13 09:56:28 +0200

    PCFileTransferer first draft.

--- MiniProject/PCFileTransferer/src/PCFileTransferer.java
+++ MiniProject/PCFileTransferer/src/PCFileTransferer.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import net.tinyos.message.Message;
 import net.tinyos.message.MessageListener;
@@ -11,45 +15,143 @@ import net.tinyos.util.PrintStreamMessenger;
 public class PCFileTransferer implements MessageListener {
 	
 	private MoteIF moteIF;
+	private LinkedBlockingQueue<short[]> dataQueue = new LinkedBlockingQueue<short[]>();
 
 	public PCFileTransferer(MoteIF moteIF) {
 		this.moteIF = moteIF;
 		this.moteIF.registerListener(new SerialDataMsg(), this);
 	}
 	
-	public void sendFile(String infilename, int moteId)
+	public void sendFile(String inFilename, int moteId)
 	{
 		SerialDataMsg payload = new SerialDataMsg();
-		//læs fil
-		
-		//while(!done)
-		//	copy data to payload.get_data()
-		//	moteIF.send(moteId, payload);		
+		FileInputStream inFile = null;
+
+		byte[] bFileChunk = new byte[SerialDataMsg.totalSize_data()];
+		long bytesSent = 0;
+		long fileSize = new File(inFilename).length();
+
+		try {
+			inFile = new FileInputStream(inFilename);
+			
+			while(bytesSent < fileSize)
+			{
+				long bytesLeft = fileSize - bytesSent;
+				int bytesToSend = 0;
+				
+				//how much to send?
+				if(bytesLeft >  bFileChunk.length)
+					bytesToSend = bFileChunk.length;
+				else
+					bytesToSend = (int) bytesLeft;
+				
+				inFile.read(bFileChunk, (int)bytesSent, bytesToSend);
+				
+				for(int i = 0; i < bytesToSend; i++)
+				{
+					payload.setElement_data(i, bFileChunk[i]);
+				}
+				
+				moteIF.send(moteId, payload);
+				
+				bytesSent += bytesToSend;
+			}			
+			
+			inFile.close();
+
+			System.out.println("Done sending " + inFilename);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void receiveFile(String outFilename, int moteId)
 	{
-		
-	}
+		FileOutputStream outFile = null;
+		try {
+			outFile = new FileOutputStream(outFilename);
+			long bytesReceived = 0;
+			
+			while(bytesReceived < 256*256) //FIXME: Hardcoded!
+			{
+				byte[] bData = null;
+				short[] sData = dataQueue.take();			
+					
+				bData = new byte[sData.length];
+				
+				for(int i = 0; i < sData.length; i++)
+				{
+					bData[i] = (byte) sData[i];
+				}
+	
+				outFile.write(bData);
+				bytesReceived += bData.length;
+			}
+			
+			outFile.flush();
+			outFile.close();
+
+			System.out.println("Done receiving " + outFilename);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
 	
 	public void messageReceived(int to, Message message) {
 		SerialDataMsg msg = (SerialDataMsg) message;
+		
+		try {
+			dataQueue.put(msg.get_data());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		System.out.println("Received serialdata packet");
 	}	
 
 	private static void usage() {
-		System.err.println("usage: PCFileTransferer [-comm <source>]");
+		System.err.println("usage: PCFileTransferer -s <sendFilePath> OR -r <receiveFilePath> [-comm <source>][-m <sendToMoteId>]");
 	}
 
 	public static void main(String[] args) throws Exception {
 		String source = null;
-		if (args.length == 2) {
-			if (!args[0].equals("-comm")) {
+		String sendFilePath = null;
+		String receiveFilePath = null;
+		int moteId = 0;
+		
+		//parse args
+		for(int i = 0; i < args.length; i++)
+		{	
+			if(args[i].equals("-s"))
+			{
+				sendFilePath = args[i+1];
+				i++;
+			}
+			else if(args[i].equals("-r"))
+			{
+				receiveFilePath = args[i+1];
+				i++;
+			}
+			else if(args[i].equals("-comm"))
+			{
+				source = args[i+1];
+				i++;
+			}
+			else if(args[i].equals("-m"))
+			{
+				moteId = Integer.parseInt(args[i+1]);
+				i++;
+			}
+			else
+			{
 				usage();
 				System.exit(1);
-			}
-			source = args[1];
-		} else if (args.length != 0) {
+			}			
+		}
+		
+		//in or outfilepath must be specified, not both
+		if(!(sendFilePath != null ^ receiveFilePath != null))
+		{
 			usage();
 			System.exit(1);
 		}
@@ -64,6 +166,13 @@ public class PCFileTransferer implements MessageListener {
 
 		MoteIF mif = new ReliableMoteIF(phoenix);
 		PCFileTransferer serial = new PCFileTransferer(mif);
+		
+		if(sendFilePath != null)
+			serial.sendFile(sendFilePath, moteId);
+		else if(receiveFilePath != null)
+		{
+			serial.receiveFile(receiveFilePath, moteId);
+		}
 		
 		//serial.sendPackets();
 	}
