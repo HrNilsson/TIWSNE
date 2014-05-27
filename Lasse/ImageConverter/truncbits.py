a6893b891123f2164c335d4e71858d77e7b83ef5
LasseBP <lasse@huhej.dk>
2014-05-27 08:50:24 +0200

    compression samples added

--- Lasse/ImageConverter/truncbits.py
+++ Lasse/ImageConverter/truncbits.py
@@ -2,13 +2,15 @@
 
 import sys
 
+trunc = int(sys.argv[2])
+
 with open(sys.argv[1],'r') as bmpRawFile:
 	fileContent = bmpRawFile.read()
 
 	fileContent = bytearray(fileContent)
 
 	for i in range(len(fileContent)):
-		fileContent[i] = (fileContent[i] >> 2) << 2			
+		fileContent[i] = (fileContent[i] >> trunc) << trunc			
 
 	with open(sys.argv[1] + 'truncated','wb') as truncFile:
 		truncFile.write(fileContent)
